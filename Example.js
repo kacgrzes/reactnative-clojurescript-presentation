09288534e99163587ade1c83697862ed69b218cf
Kacper Grzeszczyk <kacgrzes@gmail.com>
2019-02-17 14:18:30 +0100

    Add ability to change fontSize to Example component

--- Example.js
+++ Example.js
@@ -1,11 +1,14 @@
 import React, { Fragment } from 'react'
 import { Box } from 'rebass'
-import { Code } from 'mdx-deck'
+import { components } from 'mdx-deck'
 import { Split } from 'mdx-deck/layouts'
 import { flatten } from 'ramda'
 
+const H2 = components.h2
+const Code = components.code
+
 const Example = props => {
-  let { js, cljs } = props
+  let { js, cljs, fontSize = '20px' } = props
   cljs = flatten([cljs])
   js = flatten([js])
 
@@ -13,24 +16,26 @@ const Example = props => {
     <Split>
       <Fragment>
         <Box m={3}>
-        <h2>ClojureScript</h2>
-        {cljs.map(code => {
-          return <Code className={'clojure'}>
-            {code}  
-          </Code>
-        })}
-        
+          <H2>ClojureScript</H2>
+          <Box fontSize={fontSize}>
+            {cljs.map(code => {
+              return <Code className={'clojure'}>
+                {code}  
+              </Code>
+            })}
+          </Box>
         </Box>
-        <Box m={3}>
-        <h2>JavaScript</h2>
-        {js.map(code => {
-          return <Code className={'javascript'}>
-            {code}  
-          </Code>
-        })}
-        </Box>
-        
 
+        <Box m={3}>
+          <H2>JavaScript</H2>
+          <Box fontSize={fontSize}>
+            {js.map(code => {
+              return <Code className={'jsx'}>
+                {code}  
+              </Code>
+            })}
+          </Box>
+        </Box>
       </Fragment>
     </Split>
   )
