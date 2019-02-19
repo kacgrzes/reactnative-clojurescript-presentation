caaefe6f2d59942d1ef20c048d7485a38540731c
Kacper Grzeszczyk <kacgrzes@gmail.com>
2019-02-19 10:50:43 +0100

    Add simple navigation to ios demo

--- demo/src/react_up_demo/ios/core.cljs
+++ demo/src/react_up_demo/ios/core.cljs
@@ -4,6 +4,7 @@
             [react-up-demo.events]
             [react-up-demo.subs]
             [re-native.core :refer [text view image touchable-highlight app-registry react-native]]
+            [re-native.navigation :refer [create-app-container create-stack-navigator create-bottom-tab-navigator]]
             [react-up-demo.shared.ui :refer [button volume-slider]]))
 
 (def logo-img (js/require "./images/cljs.png"))
@@ -17,19 +18,33 @@
       (js/setTimeout #(swap! seconds inc) 2000)
       [text {:style {:color "green" :font-size 48}} "seconds: " @seconds])))
 
-(defn app-root []
+(defn home []
   (let [greeting (subscribe [:get-greeting])]
     (fn []
       [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
        [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} @greeting]
        [xyz]
-       [button]
-       [volume-slider { :min 0 :max 10 :thumb-tint-color "red" :minimum-track-tint-color "red" :value 5 }]
+       [button {:title "+"}]
+       [button {:title "-"}]
+       [volume-slider {:min 0 :max 10 :thumb-tint-color "red" :minimum-track-tint-color "red" :value 5}]
        [image {:source logo-img
                :style  {:width 80 :height 80 :margin-bottom 30}}]
        [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
                              :on-press #(alert "HELLO!")}
         [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me now"]]])))
+
+(def home-stack (create-stack-navigator {:home {
+                                                :screen (r/reactify-component home)
+                                                :navigation-options {
+                                                                     :title "Hello!"}}}))
+
+(def tab-router {:home (r/reactify-component home-stack)
+                 :xyz (r/reactify-component xyz)
+                 :abc (r/reactify-component xyz)})
+
+(def tab-navigator (create-bottom-tab-navigator tab-router))
+
+(defn app-root [] [:> (create-app-container tab-navigator) {}])
 
 (defn init []
   (dispatch-sync [:initialize-db])
